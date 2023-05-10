58bcf9e8039c5b1d9afa27b3e1a20bf918c0a8d6
tirahman105 <tirahman105@gmail.com>
2023-05-10 22:59:21 +0600

    firebase sign up using email and password

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,25 @@
+import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
+import { AuthContext } from '../../providers/AuthProvider';
+import { useContext } from 'react';
 
 const Login = () => {
+
+  const { signIn } = useContext(AuthContext);
+  const handleLogin = event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password)
+    signIn(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => console.log(error));
+}
+
     return (
         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
@@ -11,18 +30,18 @@ const Login = () => {
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
             <h1 className="text-3xl font-bold text-center">Login now!</h1>
-             <form>
+             <form onSubmit={handleLogin}>
              <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
+                <input type="text" name='email' placeholder="email" className="input input-bordered" />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
+                <input type="text" name='password' placeholder="password" className="input input-bordered" />
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
@@ -32,6 +51,7 @@ const Login = () => {
                 <input type="submit" value="Login" className="btn bg-[#ff3811]"/>
               </div>
              </form>
+             <p className='my-4 text-center'>New to Car Doctors ? <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
             </div>
           </div>
         </div>
